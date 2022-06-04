643595a962109dc97b8dd9b9b2775c1705a3879f
Akshit Bhatt <92199177+AkshitBhatt@users.noreply.github.com>
2022-06-04 16:49:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,34 @@ await fetch("data.json")
   .then((res) => res.json())
   .then((result) => (data = result));
 
+// document.querySelector(".work-3").innerHTML = "";
+data.map((ele, i) => {
+  document.querySelector(".work-3").innerHTML += `
+    <div class="d-flex ">
+    <div
+      class=" work1 d-flex flex-column align-items-end justify-content-end"
+    >
+      <img src=images/icon-${(i + 1).toString()}.svg class="plus" alt="" />
+
+      <div class="work">
+        <div class="d-flex justify-content-between">
+          <p>${ele.title}</p>
+          <img src="images/icon-ellipsis.svg" class="img1 mt-3" alt="" />
+        </div>
+        <p class="fs-1">${ele.timeframes.weekly.current}hrs</p>
+        <p>Last week -${ele.timeframes.weekly.previous}hrs</p>
+      </div>
+    </div>
+    `;
+});
+
 daily.addEventListener("click", () => {
   document.querySelector(".work-3").innerHTML = "";
   data.map((ele, i) => {
     document.querySelector(".work-3").innerHTML += `
-    <div class="d-flex gap-5 ">
+    <div class="d-flex">
     <div
-      class="work1 work1 d-flex flex-column align-items-end justify-content-end"
+      class=" work1 d-flex flex-column align-items-end justify-content-end"
     >
       <img src=images/icon-${(i + 1).toString()}.svg class="plus" alt="" />
 
@@ -38,9 +59,9 @@ weekly.addEventListener("click", () => {
   document.querySelector(".work-3").innerHTML = "";
   data.map((ele, i) => {
     document.querySelector(".work-3").innerHTML += `
-    <div class="d-flex gap-5">
+    <div class="d-flex ">
     <div
-      class="work1 work1 d-flex flex-column align-items-end justify-content-end"
+      class=" work1 d-flex flex-column align-items-end justify-content-end"
     >
       <img src=images/icon-${(i + 1).toString()}.svg class="plus" alt="" />
 
@@ -61,7 +82,7 @@ Monthly.addEventListener("click", () => {
   document.querySelector(".work-3").innerHTML = "";
   data.map((ele, i) => {
     document.querySelector(".work-3").innerHTML += `
-    <div class="d-flex gap-5">
+    <div class="d-flex ">
     <div
       class="work1 work1 d-flex flex-column align-items-end justify-content-end"
     >
@@ -79,3 +100,13 @@ Monthly.addEventListener("click", () => {
       `;
   });
 });
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
